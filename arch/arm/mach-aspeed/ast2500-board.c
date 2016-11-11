56e4c388173f201f5d0fa5ac75899315af491faf
Maxim Sloyko <maxims@google.com>
2016-11-10 16:28:05 -0800

    aspeed: basic scaffolding for aspeed ast2500 eval board

---
+++ arch/arm/mach-aspeed/ast2500-board.c
@@ -0,0 +1,37 @@
+#include <common.h>
+#include <asm/io.h>
+
+#define AST_TIMER_BASE			(0x1e782000)
+#define AST_WDT_BASE			(0x1e785000)
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void lowlevel_init(void)
+{
+}
+
+int board_early_init_f(void)
+{
+	writel(0xae, AST_TIMER_BASE + 0x38);
+	writel(0xf000, AST_TIMER_BASE + 0x3c);
+	writel(~0, AST_TIMER_BASE + 0x44);
+	writel((3 << 12), AST_TIMER_BASE + 0x30);
+#ifndef CONFIG_FIRMWARE_2ND_BOOT
+	writel(0, AST_WDT_BASE + 0x2c);
+#endif
+	writel(0, AST_WDT_BASE + 0x4c);
+	return 0;
+}
+
+int board_init(void)
+{
+	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x100;
+	return 0;
+}
+
+int dram_init(void)
+{
+	// Huge TODO
+	return 0;
+}
+
---
+++ include/configs/evb_ast2500.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2012-2020  ASPEED Technology Inc.
+ * Ryan Chen <ryan_chen@aspeedtech.com>
+ *
+ * Copyright 2016 Google Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <configs/aspeed-common.h>
+
+
+#define CONFIG_SYS_MEMTEST_START		CONFIG_SYS_SDRAM_BASE + 0x300000
+#define CONFIG_SYS_MEMTEST_END			(CONFIG_SYS_MEMTEST_START + (80*1024*1024))
+
+#define CONFIG_SYS_UBOOT_BASE		CONFIG_SYS_TEXT_BASE
+/*
+ * Memory Info
+ */
+#define CONFIG_SYS_MALLOC_LEN   	(0x1000 + 4*1024*1024) /* malloc() len */
+
+#define CONFIG_SYS_LOAD_ADDR	0x83000000	/* default load address */
+
+#define CONFIG_BOARD_EARLY_INIT_F
+
+#endif	/* __CONFIG_H */
