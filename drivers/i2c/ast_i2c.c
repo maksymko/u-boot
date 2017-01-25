5e64647a5af080825883f5b6167ff5cfd55d0c25
Maxim Sloyko <maxims@google.com>
2017-01-25 10:42:25 -0800

    aspeed/i2c: Refactored to use priv/regs naming

--- drivers/i2c/ast_i2c.c
+++ drivers/i2c/ast_i2c.c
@@ -7,15 +7,20 @@
  */
 
 #include <common.h>
+#include <clk.h>
 #include <dm.h>
 #include <errno.h>
 #include <fdtdec.h>
 #include <i2c.h>
+#include <reset.h>
 #include <asm/io.h>
+
 #include "ast_i2c.h"
 
 #define I2C_TIMEOUT_US 100000
 #define I2C_SLEEP_STEP_US 20
+
+#define HIGHSPEED_TTIMEOUT		3
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -23,6 +28,8 @@ DECLARE_GLOBAL_DATA_PTR;
  * Device private data
  */
 struct ast_i2c_priv {
+	/* This device's clock */
+	struct clk clk;
 	/* Device registers */
 	struct ast_i2c_regs *regs;
 	/* I2C speed in Hz */
@@ -53,9 +60,11 @@ static u32 get_clk_reg_val(ulong divider_ratio)
 	return data;
 }
 
-static void ast_i2c_clear_interrupts(struct ast_i2c_regs *i2c_base)
+static void ast_i2c_clear_interrupts(struct udevice *dev)
 {
-	writel(~0, &i2c_base->isr);
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
+
+	writel(~0, &priv->regs->isr);
 }
 
 static void ast_i2c_init_bus(struct udevice *dev)
@@ -65,12 +74,12 @@ static void ast_i2c_init_bus(struct udevice *dev)
 	/* Reset device */
 	writel(0, &priv->regs->fcr);
 	/* Enable Master Mode. Assuming single-master */
-	debug("Enable Master for %p\n", i2c_bus->regs);
+	debug("Enable Master for %p\n", priv->regs);
 	writel(I2CD_MASTER_EN
 	       | I2CD_M_SDA_LOCK_EN
 	       | I2CD_MULTI_MASTER_DIS | I2CD_M_SCL_DRIVE_EN,
 	       &priv->regs->fcr);
-	debug("FCR: %p\n", &priv->regs->fcr);
+	debug("FCR: %p\n", readl(&priv->regs->fcr));
 	/* Enable Interrupts */
 	writel(I2CD_INTR_TX_ACK
 	       | I2CD_INTR_TX_NAK
@@ -83,59 +92,88 @@ static void ast_i2c_init_bus(struct udevice *dev)
 static int ast_i2c_ofdata_to_platdata(struct udevice *dev)
 {
 	struct ast_i2c_priv *priv = dev_get_priv(dev);
-	struct ast_i2c_regs *regs = dev_get_addr_ptr(dev);
+	int ret;
 
-	if (IS_ERR(regs))
-		return PTR_ERR(regs);
+	priv->regs = dev_get_addr_ptr(dev);
+	if (IS_ERR(priv->regs))
+		return PTR_ERR(priv->regs);
 
-	priv->regs = i2c_base;
+	ret = clk_get_by_index(dev, 0, &priv->clk);
+	if (ret < 0) {
+		debug("%s: Can't get clock for %s: %d\n", __func__, dev->name, ret);
+		return ret;
+	}
 
 	return 0;
 }
 
 static int ast_i2c_probe(struct udevice *dev)
 {
-	struct ast_i2c_priv *priv = dev_get_priv(dev);
-
 	debug("Enabling I2C%u\n", dev->seq);
-
 	ast_i2c_init_bus(dev);
 
 	return 0;
 }
 
-static inline int ast_i2c_wait_isr(struct ast_i2c_regs *i2c_base, u32 flag)
+static int ast_i2c_ctrl_probe(struct udevice *dev)
 {
+	struct reset_ctl reset_ctl;
+	int ret;
+
+	debug("%s\n", __func__);
+	/* Reset all I2C devices */
+
+	ret = reset_get_by_index(dev, 0, &reset_ctl);
+	if (ret) {
+		debug("%s(): Failed to get reset signal\n", __func__);
+		return ret;
+	}
+
+	ret = reset_assert(&reset_ctl);
+	if (ret) {
+		debug("%s(): I2C reset failed: %u\n", __func__, ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int ast_i2c_wait_isr(struct udevice *dev, u32 flag)
+{
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
 	int timeout = I2C_TIMEOUT_US;
 
-	while (!(readl(&i2c_base->isr) & flag) && timeout > 0) {
+	while (!(readl(&priv->regs->isr) & flag) && timeout > 0) {
 		udelay(I2C_SLEEP_STEP_US);
 		timeout -= I2C_SLEEP_STEP_US;
 	}
 
-	ast_i2c_clear_interrupts(i2c_base);
+	ast_i2c_clear_interrupts(dev);
 	if (timeout <= 0)
 		return -ETIMEDOUT;
 
 	return 0;
 }
 
-static inline int ast_i2c_send_stop(struct ast_i2c_regs *i2c_base)
+static int ast_i2c_send_stop(struct udevice *dev)
 {
-	writel(I2CD_M_STOP_CMD, &i2c_base->csr);
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
 
-	return ast_i2c_wait_isr(i2c_base, I2CD_INTR_NORMAL_STOP);
+	writel(I2CD_M_STOP_CMD, &priv->regs->csr);
+
+	return ast_i2c_wait_isr(dev, I2CD_INTR_NORMAL_STOP);
 }
 
-static inline int ast_i2c_wait_tx(struct ast_i2c_regs *i2c_base)
+static int ast_i2c_wait_tx(struct udevice *dev)
 {
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
 	int timeout = I2C_TIMEOUT_US;
 	u32 flag = I2CD_INTR_TX_ACK | I2CD_INTR_TX_NAK;
-	u32 status = readl(&i2c_base->isr) & flag;
+	u32 status = readl(&priv->regs->isr) & flag;
 	int ret = 0;
 
 	while (!status && timeout > 0) {
-		status = readl(&i2c_base->isr) & flag;
+		status = readl(&priv->regs->isr) & flag;
 		udelay(I2C_SLEEP_STEP_US);
 		timeout -= I2C_SLEEP_STEP_US;
 	}
@@ -146,80 +184,81 @@ static inline int ast_i2c_wait_tx(struct ast_i2c_regs *i2c_base)
 	if (timeout <= 0)
 		ret = -ETIMEDOUT;
 
-	ast_i2c_clear_interrupts(i2c_base);
+	ast_i2c_clear_interrupts(dev);
 
 	return ret;
 }
 
-static int ast_i2c_start_txn(struct ast_i2c_regs *i2c_base, uint devaddr)
+static int ast_i2c_start_txn(struct udevice *dev, uint devaddr)
 {
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
 	/* Start and Send Device Address */
-	writel(devaddr, &i2c_base->trbbr);
-	writel(I2CD_M_START_CMD | I2CD_M_TX_CMD, &i2c_base->csr);
+	writel(devaddr, &priv->regs->trbbr);
+	writel(I2CD_M_START_CMD | I2CD_M_TX_CMD, &priv->regs->csr);
 
-	return ast_i2c_wait_tx(i2c_base);
+	return ast_i2c_wait_tx(dev);
 }
 
-static int ast_i2c_read_data(struct ast_i2c *i2c_bus, u8 chip_addr, u8 *buffer,
+static int ast_i2c_read_data(struct udevice *dev, u8 chip_addr, u8 *buffer,
 			     size_t len, bool send_stop)
 {
-	struct ast_i2c_regs *i2c_base = i2c_bus->regs;
-	int i2c_error =
-	    ast_i2c_start_txn(i2c_base, (chip_addr << 1) | I2C_M_RD);
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
 	u32 i2c_cmd = I2CD_M_RX_CMD;
+	int ret;
 
-	if (i2c_error < 0)
-		return i2c_error;
+	ret = ast_i2c_start_txn(dev, (chip_addr << 1) | I2C_M_RD);
+	if (ret < 0)
+		return ret;
 
 	for (; len > 0; len--, buffer++) {
 		if (len == 1)
 			i2c_cmd |= I2CD_M_S_RX_CMD_LAST;
-		writel(i2c_cmd, &i2c_base->csr);
-		i2c_error = ast_i2c_wait_isr(i2c_base, I2CD_INTR_RX_DONE);
-		if (i2c_error < 0)
-			return i2c_error;
-		*buffer = (readl(&i2c_base->trbbr) & I2CD_RX_DATA_MASK)
+		writel(i2c_cmd, &priv->regs->csr);
+		ret = ast_i2c_wait_isr(dev, I2CD_INTR_RX_DONE);
+		if (ret < 0)
+			return ret;
+		*buffer = (readl(&priv->regs->trbbr) & I2CD_RX_DATA_MASK)
 				>> I2CD_RX_DATA_SHIFT;
 	}
-	ast_i2c_clear_interrupts(i2c_base);
+	ast_i2c_clear_interrupts(dev);
 
 	if (send_stop)
-		return ast_i2c_send_stop(i2c_base);
+		return ast_i2c_send_stop(dev);
 
 	return 0;
 }
 
-static int ast_i2c_write_data(struct ast_i2c *i2c_bus, u8 chip_addr, u8
+static int ast_i2c_write_data(struct udevice *dev, u8 chip_addr, u8
 			      *buffer, size_t len, bool send_stop)
 {
-	struct ast_i2c_regs *i2c_base = i2c_bus->regs;
-	int i2c_error = ast_i2c_start_txn(i2c_base, (chip_addr << 1));
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
+	int ret;
 
-	if (i2c_error < 0)
-		return i2c_error;
+	ret = ast_i2c_start_txn(dev, (chip_addr << 1));
+	if (ret < 0)
+		return ret;
 
 	for (; len > 0; len--, buffer++) {
-		writel(*buffer, &i2c_base->trbbr);
-		writel(I2CD_M_TX_CMD, &i2c_base->csr);
-		i2c_error = ast_i2c_wait_tx(i2c_base);
-		if (i2c_error < 0)
-			return i2c_error;
+		writel(*buffer, &priv->regs->trbbr);
+		writel(I2CD_M_TX_CMD, &priv->regs->csr);
+		ret = ast_i2c_wait_tx(dev);
+		if (ret < 0)
+			return ret;
 	}
 
 	if (send_stop)
-		return ast_i2c_send_stop(i2c_base);
+		return ast_i2c_send_stop(dev);
 
 	return 0;
 }
 
 static int ast_i2c_deblock(struct udevice *dev)
 {
-	struct ast_i2c *i2c_bus = dev_get_priv(dev);
-	struct ast_i2c_regs *i2c_base = i2c_bus->regs;
-	u32 csr = readl(&i2c_base->csr);
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
+	struct ast_i2c_regs *regs = priv->regs;
+	u32 csr = readl(&regs->csr);
 	bool sda_high = csr & I2CD_SDA_LINE_STS;
 	bool scl_high = csr & I2CD_SCL_LINE_STS;
-
 	int ret = 0;
 
 	if (sda_high && scl_high) {
@@ -228,15 +267,15 @@ static int ast_i2c_deblock(struct udevice *dev)
 	} else if (sda_high) {
 		/* Send stop command */
 		debug("Unterminated TXN in (%x), sending stop\n", csr);
-		ret = ast_i2c_send_stop(i2c_base);
+		ret = ast_i2c_send_stop(dev);
 	} else if (scl_high) {
 		/* Possibly stuck slave */
 		debug("Bus stuck (%x), attempting recovery\n", csr);
-		writel(I2CD_BUS_RECOVER_CMD, &i2c_base->csr);
-		ret = ast_i2c_wait_isr(i2c_base, I2CD_INTR_BUS_RECOVER_DONE);
+		writel(I2CD_BUS_RECOVER_CMD, &regs->csr);
+		ret = ast_i2c_wait_isr(dev, I2CD_INTR_BUS_RECOVER_DONE);
 	} else {
 		/* Just try to reinit the device. */
-		ast_i2c_init_bus(i2c_bus);
+		ast_i2c_init_bus(dev);
 	}
 
 	return ret;
@@ -244,9 +283,9 @@ static int ast_i2c_deblock(struct udevice *dev)
 
 static int ast_i2c_xfer(struct udevice *dev, struct i2c_msg *msg, int nmsgs)
 {
-	struct ast_i2c *i2c_bus = dev_get_priv(dev);
-	int ret = ast_i2c_deblock(dev);
+	int ret;
 
+	ret = ast_i2c_deblock(dev);
 	if (ret < 0)
 		return ret;
 	debug("i2c_xfer: %d messages\n", nmsgs);
@@ -254,12 +293,12 @@ static int ast_i2c_xfer(struct udevice *dev, struct i2c_msg *msg, int nmsgs)
 		if (msg->flags & I2C_M_RD) {
 			debug("i2c_read: chip=0x%x, len=0x%x, flags=0x%x\n",
 			      msg->addr, msg->len, msg->flags);
-			ret = ast_i2c_read_data(i2c_bus, msg->addr, msg->buf,
+			ret = ast_i2c_read_data(dev, msg->addr, msg->buf,
 						msg->len, (nmsgs == 1));
 		} else {
 			debug("i2c_write: chip=0x%x, len=0x%x, flags=0x%x\n",
 			      msg->addr, msg->len, msg->flags);
-			ret = ast_i2c_write_data(i2c_bus, msg->addr, msg->buf,
+			ret = ast_i2c_write_data(dev, msg->addr, msg->buf,
 						 msg->len, (nmsgs == 1));
 		}
 		if (ret) {
@@ -273,34 +312,33 @@ static int ast_i2c_xfer(struct udevice *dev, struct i2c_msg *msg, int nmsgs)
 
 static int ast_i2c_set_speed(struct udevice *dev, unsigned int speed)
 {
-	struct ast_i2c *i2c_bus = dev_get_priv(dev);
-	struct ast_i2c_regs *i2c_base = i2c_bus->regs;
-	ulong pclk, divider;
+	struct ast_i2c_priv *priv = dev_get_priv(dev);
+	struct ast_i2c_regs *regs = priv->regs;
+	ulong i2c_rate, divider;
 
-	debug("Setting speed ofr I2C%d to <%u>\n", dev->seq, speed);
+	debug("Setting speed for I2C%d to <%u>\n", dev->seq, speed);
 	if (!speed) {
 		debug("No valid speed specified\n");
 		return -EINVAL;
 	}
 
-	pclk = ast_get_apbclk();
-	divider = pclk / speed;
+	i2c_rate = clk_get_rate(&priv->clk);
+	divider = i2c_rate / speed;
 
-	i2c_bus->speed = speed;
-	if (speed > 400000) {
-		debug("Enabling High Speed\n");
-		setbits_le32(&i2c_base->fcr, I2CD_M_HIGH_SPEED_EN
+	priv->speed = speed;
+	if (speed > I2C_HIGHSPEED_RATE) {
+		debug("Enable High Speed\n");
+		setbits_le32(&regs->fcr, I2CD_M_HIGH_SPEED_EN
 			     | I2CD_M_SDA_DRIVE_1T_EN
 			     | I2CD_SDA_DRIVE_1T_EN);
-		writel(0x3, &i2c_base->cactcr2);
-		writel(get_clk_reg_val(divider), &i2c_base->cactcr1);
+		writel(HIGHSPEED_TTIMEOUT, &regs->cactcr2);
 	} else {
 		debug("Enabling Normal Speed\n");
-		writel(get_clk_reg_val(divider), &i2c_base->cactcr1);
-		writel(I2CD_NO_TIMEOUT_CTRL, &i2c_base->cactcr2);
+		writel(I2CD_NO_TIMEOUT_CTRL, &regs->cactcr2);
 	}
 
-	ast_i2c_clear_interrupts(i2c_base);
+	writel(get_clk_reg_val(divider), &regs->cactcr1);
+	ast_i2c_clear_interrupts(dev);
 
 	return 0;
 }
@@ -309,6 +347,9 @@ static const struct dm_i2c_ops ast_i2c_ops = {
 	.xfer = ast_i2c_xfer,
 	.set_bus_speed = ast_i2c_set_speed,
 	.deblock = ast_i2c_deblock,
+};
+
+static const struct dm_i2c_ops ast_i2c_ctrl_ops = {
 };
 
 static const struct udevice_id ast_i2c_ids[] = {
@@ -339,4 +380,5 @@ U_BOOT_DRIVER(ast_i2c_ctrl) = {
 	.id = UCLASS_I2C,
 	.of_match = ast_i2c_ctrl_ids,
 	.probe = ast_i2c_ctrl_probe,
+	.ops = &ast_i2c_ctrl_ops,
 };
